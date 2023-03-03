0c63090e16308bf382837c4e206c0ae07ecfb28a
Limgyuho <79262519+Limgyuho@users.noreply.github.com>
2023-03-03 20:58:41 +0900

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,12 @@
+var divs = document.getElementsByTagName('div');
+
+function a() {
+    // 구현
+    divs[0].style.backgroundColor = 'red';
+    divs[1].style.backgroundColor = 'Orange';
+    divs[2].style.backgroundColor = 'yellow';
+    divs[3].style.backgroundColor = 'green';
+    divs[4].style.backgroundColor = 'blue';
+    divs[5].style.backgroundColor = 'navy';
+    divs[6].style.backgroundColor = 'purple';
+}
